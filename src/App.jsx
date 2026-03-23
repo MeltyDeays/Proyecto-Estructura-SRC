e13fe022ba6f3b1cb2e5b19a14b97968bdc4c6e5
MeltyDeays <everdavicloez123@gmail.com>
2026-03-23 15:30:41 -0600

    Probando Estructura

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Encabezado from "./components/navegacion/Encabezado";
+
+import Inicio from "./view/Inicio";
+import Categorias from "./view/Categorias";
+import Catalogo from "./view/Catalogo";
+import Productos from "./view/Productos";
+import Login from "./view/Login";
+import RutaProtegida from "./components/rutas/RutaProtegida";
+import Pagina404 from "./view/Pagina404";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <Router>
+      <Encabezado />
+      <div className="margin-superior-main">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<RutaProtegida><Inicio /></RutaProtegida>} />
+          <Route path="/categorias" element={<RutaProtegida><Categorias /></RutaProtegida>} />
+          <Route path="/catalogo" element={<Catalogo />} />
+          <Route path="/productos" element={<RutaProtegida><Productos /></RutaProtegida>} />
+          <Route path="*" element={<Pagina404 />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
